16b9e0452980c96beb70f68d8d88f0e9cf87c45b
Daniel Rodrigues <danrod278@gmail.com>
2023-06-12 21:48:23 -0400

    ok

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+const path = require("path")
+
+function criarPasta(nome){
+    fs.mkdir(path.join(__dirname, `${nome}`), (erro)=>{
+        if(erro){
+            console.log("Houve um erro ao criar a pasta: " + erro)
+            return
+        }
+        console.log("Pasta criada com sucesso!")
+    })
+}
+
+function criarDataBase(caminho, pasta, nome){
+    var caminho_database = path.join(pasta, "/"+nome+".json")
+    fs.writeFileSync(`${caminho_database}`, "", "utf-8", (err)=>{
+        if(err){
+            console.log(`Houve um erro ao criar o arquivo ${caminho_database}:       `+err)
+            return
+        }
+        console.log(`Arquivo ${caminho_database}.json criado com sucesso`)
+    })
+}
+ 
+var pasta = "teste5"
+var arquivo = "teste6"
+
+criarPasta(pasta)
+criarDataBase(__dirname, pasta, arquivo)
+//criarDataBase(caminho, pasta, arquivo/**/)
