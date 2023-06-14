cee45414294782b1f79d0e393329f87e6da13273
Daniel Rodrigues <danrod278@gmail.com>
2023-06-13 22:24:47 -0400

    caimbra no pescoço

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs")
 const path = require("path")
-
 function criarPasta(nome){
     fs.mkdir(path.join(__dirname, `${nome}`), (erro)=>{
         if(erro){
@@ -10,29 +9,48 @@ function criarPasta(nome){
         console.log("Pasta criada com sucesso!")
     })
 }
+function criarDataBase(pasta, nome_arquivo, nome_database){
+    const obj_inicial = `{
+        "${nome_database}":{
 
-function criarDataBase(caminho, pasta, nome){
-    const coisa_inicial = `{"${nome}":{}
+    }
 }`
-    var caminho_database = path.join(pasta, "/"+nome+".json")
-    fs.writeFileSync(`${caminho_database}`, "", "utf-8", (err)=>{
-        if(err){
-            console.log(caminho_database+err)
-            return
-        }
-        
-    })
-    fs.writeFile(caminho_database, coisa_inicial, "utf-8", (err)=>{
+    var caminho_database = path.join(pasta, "/"+nome_arquivo+".json")
+    console.log(caminho_database)
+    fs.writeFileSync(`${caminho_database}`, "", "utf-8")
+
+    fs.writeFile(caminho_database, obj_inicial, "utf-8", (err)=>{
         if(err){
             console.log(`Houve um erro ao criar o arquivo ${caminho_database}:       `+err)
             return
         }
         console.log(`Arquivo ${caminho_database}.json criado com sucesso`)    })
 }
- 
-var pasta = "teste5"
-var arquivo = "teste6"
+function add_obj(pasta, nome_arquivo, nome_database, objeto) {
+    try {
+        var caminho = `${pasta}/${nome_arquivo}.json`
+        fs.readFile(caminho, 'utf-8',(err, dado)=>{
+            if(err){
+                    console.log("Houve um erro                   ." +err)
+                    return
+            }try{
+                const arquivo = JSON.parse(dado) 
+                arquivo[nome_database][objeto] = {};
 
-criarPasta(pasta)
-criarDataBase(__dirname, pasta, arquivo)
-//criarDataBase(caminho, pasta, arquivo/**/)
+                fs.writeFile(`${pasta}/${nome_arquivo}.json`, JSON.stringify(arquivo, null, 2), '', (err) => {
+                    if (err) {
+                        console.log('Erro ao criar objeto:', err);
+                        return;
+                    }
+                    console.log('Objeto criado com sucesso!');
+                })
+            }
+            catch(erro){
+                console.log("Huve um erro 2                 ."+erro)
+            }
+            })
+        } 
+    catch (err) {
+        console.log('Erro ao ler ou manipular o arquivo:', err);
+    }  
+}
